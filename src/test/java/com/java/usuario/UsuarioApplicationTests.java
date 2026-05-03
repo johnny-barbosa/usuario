20515cd338e05a42ab6899c0722a8fef04361a17
Johnny Barbosa Macedo <johnnygouki@gmail.com>
2026-05-03 10:52:36 -0300

    Commit inicial

---
+++ src/test/java/com/java/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
